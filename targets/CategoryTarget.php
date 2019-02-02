38ecf6fb19de9321941941c0f5ec17bf069acf3f
Zhao vistart <i@vistart.me>
2019-02-02 18:35:00 +0800

    Create Targets.

---
+++ targets/CategoryTarget.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ *  _   __ __ _____ _____ ___  ____  _____
+ * | | / // // ___//_  _//   ||  __||_   _|
+ * | |/ // /(__  )  / / / /| || |     | |
+ * |___//_//____/  /_/ /_/ |_||_|     |_|
+ * @link https://vistart.name/
+ * @copyright Copyright (c) 2016-2019 vistart
+ * @license https://vistart.name/license/
+ */
+
+namespace rhoone\library\providers\huiwen\targets\tongjiuniversity\targets;
+
+use rhoone\library\providers\huiwen\targets\CategoryTarget as baseCategoryTarget;
+
+/**
+ * Class CategoryTarget
+ * @package rhoone\library\targets
+ * @author vistart <i@vistart.me>
+ */
+class CategoryTarget extends baseCategoryTarget
+{
+    public $relativeUrl = "/browse/cls_browsing.php";
+}
---
+++ targets/MarcTarget.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ *  _   __ __ _____ _____ ___  ____  _____
+ * | | / // // ___//_  _//   ||  __||_   _|
+ * | |/ // /(__  )  / / / /| || |     | |
+ * |___//_//____/  /_/ /_/ |_||_|     |_|
+ * @link https://vistart.name/
+ * @copyright Copyright (c) 2016-2019 vistart
+ * @license https://vistart.name/license/
+ */
+
+namespace rhoone\library\providers\huiwen\targets\tongjiuniversity\targets;
+
+use rhoone\library\providers\huiwen\targets\MarcTarget as baseMarcTarget;
+
+/**
+ * Class MarcTarget
+ * @package rhoone\library\targets
+ * @author vistart <i@vistart.me>
+ */
+class MarcTarget extends baseMarcTarget
+{
+    public $relativeUrl = "/opac/item.php";
+}
