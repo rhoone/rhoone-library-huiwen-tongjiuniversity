5cd806d5abce677d8bd62fc35bc9319d21199132
vistart <i@vistart.me>
2019-03-07 10:48:18 +0800

    Create BatchDownloadToMongoDBJob.php

---
+++ job/BatchDownloadToMongoDBJob.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ *  _   __ __ _____ _____ ___  ____  _____
+ * | | / // // ___//_  _//   ||  __||_   _|
+ * | |/ // /(__  )  / / / /| || |     | |
+ * |___//_//____/  /_/ /_/ |_||_|     |_|
+ * @link https://vistart.name/
+ * @copyright Copyright (c) 2016-2019 vistart
+ * @license https://vistart.name/license/
+ */
+
+namespace rhoone\library\providers\huiwen\targets\tongjiuniversity\job;
+
+use \rhoone\library\providers\huiwen\job\BatchDownloadToMongoDBJob as baseJob;
+
+/**
+ * Class BatchDownloadToMongoDBJob
+ * @package rhoone\library\providers\huiwen\targets\tongjiuniversity\job
+ */
+class BatchDownloadToMongoDBJob extends baseJob
+{
+
+}
