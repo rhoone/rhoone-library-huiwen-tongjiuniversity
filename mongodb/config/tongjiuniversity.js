287bdc89ba41c6152ad13928849f330beaa5c7d1
Zhao vistart <i@vistart.me>
2019-03-08 13:07:11 +0800

    Update tongjiuniversity.js
    add two indexes.

--- mongodb/config/tongjiuniversity.js
+++ mongodb/config/tongjiuniversity.js
@@ -6,4 +6,6 @@ db.createUser({
 	}]
 });
 
-db.downloaded_content.createIndex({"marc_no": 1}, {name: "marc_no_unique_asc", unique: true})
+db.downloaded_content.createIndex({"marc_no": 1}, {name: "marc_no_unique_asc", unique: true});
+db.downloaded_content.createIndex({"created_at": 1}, {name: "created_at_asc"});
+db.downloaded_content.createIndex({"updated_at": 1}, {name: "updated_at_asc"});
