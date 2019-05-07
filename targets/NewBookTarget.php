989a30fb92848b1d2e932e444e6acdb84d22656a
Zhao vistart <i@vistart.me>
2019-05-07 20:24:41 +0800

    Create NewBookTarget.php

---
+++ targets/NewBookTarget.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ *  _   __ __ _____ _____ ___  ____  _____
+ * | | / // // ___//_  _//   ||  __||_   _|
+ * | |/ // /(__  )  / / / /| || |     | |
+ * |___//_//____/  /_/ /_/ |_||_|     |_|
+ * @link https://vistart.name/
+ * @copyright Copyright (c) 2016-2019 vistart
+ * @license https://vistart.name/license/
+ */
+namespace rhoone\library\providers\huiwen\targets\tongjiuniversity\targets;
+
+/**
+ * Class NewBookTarget
+ * @package rhoone\library\providers\huiwen\targets\tongjiuniversity\targets
+ */
+class NewBookTarget extends \rhoone\library\providers\huiwen\targets\NewBookTarget
+{
+    /**
+     * @var string
+     */
+    public $host = "webpac.lib.tongji.edu.cn";
+}
